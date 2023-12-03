c5ea7453b2c7e7acd7de2f396b1a73a251416d3a
Valentin Cernuta <vcernuta3@gmail.com>
2023-12-03 12:50:55 +0100

    day 3 part 1

--- src/aoc_2023.nim
+++ src/aoc_2023.nim
@@ -1,7 +1,8 @@
 import strutils
 import days/day1
 import days/day2
+import days/day3
 
 when isMainModule:
-    echo "Part 1: " & intToStr(day2.part1())
-    echo "Part 2: " & intToStr(day2.part2())
+    echo "Part 1: " & intToStr(day3.part1())
+    echo "Part 2: " & intToStr(day3.part2())
---
+++ src/days/day3.nim
@@ -0,0 +1,50 @@
+import strutils
+import sequtils
+import re
+
+proc parseInput(): string =
+    let file = "./inputs/3.txt"
+    let handle = file.open()
+
+    result = handle.readAll()
+    handle.close()
+
+proc part1*(): int =
+    let baseInput = parseInput()
+    let lineLength = baseInput.find('\n')
+    let input = baseInput.replace("\n", "")
+
+    var counter = 0
+    var boundsStart = 0
+    while true:
+        let (first, last) = input.findBounds(re"\d+", start = boundsStart)
+        if first < 0:
+            break
+
+        let length = (last - first) + 1
+        let identifier = parseInt(input.substr(first, last))
+
+        var positions = @[
+            first - 1, # left
+            first - 1 - lineLength, # top left bound
+            last + 1, # right
+            last + 1 + lineLength, # bottom right bound
+        ]
+
+        for i in 0..length + 1:
+            positions.add(first - 1 - lineLength + i) # Up line
+            positions.add(last + 1 + lineLength - i) # Down line
+
+        positions = positions.filterIt(it >= 0 and it <= input.len - 1).deduplicate()
+        
+        for position in positions:
+            let element = $input[position]
+            if element != "." and not element.match(re"\d"):
+                counter += identifier
+
+        boundsStart = last + 1
+
+    return counter
+
+proc part2*(): int =
+    return 0

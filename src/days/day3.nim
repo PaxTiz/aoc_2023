83ee3c568c6ed5213e45eef4ba41f6af3c3673bc
Valentin Cernuta <vcernuta3@gmail.com>
2023-12-03 13:04:30 +0100

    day 2 part 2

--- src/days/day3.nim
+++ src/days/day3.nim
@@ -1,5 +1,6 @@
 import strutils
 import sequtils
+import tables
 import re
 
 proc parseInput(): string =
@@ -47,4 +48,47 @@ proc part1*(): int =
     return counter
 
 proc part2*(): int =
-    return 0
+    let baseInput = parseInput()
+    let lineLength = baseInput.find('\n')
+    let input = baseInput.replace("\n", "")
+
+    var starsToIds = initTable[int, seq[int]]()
+
+    var counter = 0
+    var boundsStart = 0
+    while true:
+        let (first, last) = input.findBounds(re"\d+", start = boundsStart)
+        if first < 0:
+            break
+
+        let length = (last - first) + 1
+        let identifier = parseInt(input.substr(first, last))
+
+        var positions = @[
+            first - 1, # left
+            first - 1 - lineLength, # top left bound
+            last + 1, # right
+            last + 1 + lineLength, # bottom right bound
+        ]
+
+        for i in 0..length + 1:
+            positions.add(first - 1 - lineLength + i) # Up line
+            positions.add(last + 1 + lineLength - i) # Down line
+
+        positions = positions.filterIt(it >= 0 and it <= input.len - 1).deduplicate()
+        
+        for position in positions:
+            let element = $input[position]
+            if element == "*":
+                if not starsToIds.hasKey(position):
+                    starsToIds[position] = @[]
+    
+                starsToIds[position].add(identifier)
+
+        boundsStart = last + 1
+
+    for (position, ids) in starsToIds.pairs():
+        if ids.len == 2:
+            counter += ids[0] * ids[1]
+
+    return counter

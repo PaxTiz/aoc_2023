7b782733fb247dea0c6e1c05fafc989e1c1c96cd
Valentin Cernuta <vcernuta3@gmail.com>
2023-12-05 21:35:30 +0100

    day 5 part 2

--- src/days/day5.nim
+++ src/days/day5.nim
@@ -51,4 +51,46 @@ proc part1*(): int =
         result = min(result, currentIndex)
 
 proc part2*(): int =
-    return 0
+    let input = parseInput()
+    let initialSeeds = input[0].split(": ")[1].splitWhitespace().mapIt(parseInt(it))
+
+    var seeds = newSeq[int]()
+    let pairs = initialSeeds.distribute(2)
+    for pair in pairs:
+        for i in pair[0]..pair[0] + pair[1]:
+            seeds.add(i)
+    
+    var parsedResults: Numbers = @[]
+    var wasNewline = true
+    var currentArray = newSeq[Ranges]()
+    for lineIndex in 1 .. input.len - 1:
+        let line = input[lineIndex]
+        if lineIndex == input.len - 1:
+            let currentLineNumbers = line.splitWhitespace().mapIt(parseInt(it))
+            currentArray.add((currentLineNumbers[0], currentLineNumbers[1], currentLineNumbers[2]))
+            parsedResults.add(currentArray)
+
+        if line == "\n" or line.len == 0:
+            parsedResults.add(currentArray)
+            currentArray = @[]
+            wasNewline = true
+            continue
+
+        if wasNewline:
+            wasNewline = false
+            continue
+
+        let currentLineNumbers = line.splitWhitespace().mapIt(parseInt(it))
+        currentArray.add((currentLineNumbers[0], currentLineNumbers[1], currentLineNumbers[2]))
+
+    result = high(int)
+    for seed in seeds:
+        var currentIndex = seed
+        for values in parsedResults:
+            for items in values:
+                if currentIndex >= items[1] and currentIndex <= items[1] + items[2]:
+                    let position = (items[1] + items[2]) - currentIndex
+                    currentIndex = (items[0] + items[2]) - position
+                    break
+
+        result = min(result, currentIndex)

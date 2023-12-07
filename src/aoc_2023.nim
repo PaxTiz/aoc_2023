8a78589a33aa8918d12a5c9af0f806a16c2afbb4
Valentin Cernuta <vcernuta3@gmail.com>
2023-12-07 18:17:12 +0100

    day 6 part 1

--- src/aoc_2023.nim
+++ src/aoc_2023.nim
@@ -4,7 +4,8 @@ import days/day2
 import days/day3
 import days/day4
 import days/day5
+import days/day6
 
 when isMainModule:
-    echo "Part 1: " & intToStr(day5.part1())
-    echo "Part 2: " & intToStr(day5.part2())
+    echo "Part 1: " & intToStr(day6.part1())
+    echo "Part 2: " & intToStr(day6.part2())
---
+++ src/days/day6.nim
@@ -0,0 +1,39 @@
+import strutils
+import sequtils
+
+type Race = tuple[time: int, distance: int]
+
+proc parseInput(): seq[Race] =
+  result = newSeq[Race]()
+  let file = "./inputs/6.txt"
+  let handle = file.open()
+
+  let lines = handle.readAll().splitLines()
+  handle.close()
+
+  let timesLine = lines[0].split(" ")
+  let times = timesLine[1..timesLine.len - 1].mapIt(it.strip()).filterIt(it.len > 0).mapIt(parseInt(it))
+
+  let distancesLine = lines[1].split(" ")
+  let distances = distancesLine[1..distancesLine.len - 1].mapIt(it.strip()).filterIt(it.len > 0).mapIt(parseInt(it))
+
+  result = times.zip(distances)
+
+proc part1*(): int =
+    let races = parseInput()
+
+    result = 1
+    for race in races:
+      let raceDuration = race.time
+      let raceDistance = race.distance
+
+      var possibilities = 0
+      for i in 1..raceDuration:
+        let distance = (raceDuration - i) * i
+        if distance > raceDistance:
+          possibilities += 1
+
+      result *= possibilities
+
+proc part2*(): int =
+    return 0
